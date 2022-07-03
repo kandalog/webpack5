b1a820860abea8d01af44948e69f1edb779ff804
kandalog <asuasu.tee@gmail.com>
2022-07-04 00:03:00 +0900

    webpack5で基本的な設定完了

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "js/main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|jpg)/,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
